fe0a1d5d617be11050165ed3297c1463ea769e60
shirinzarqaa <shirin.zarqaa@ui.ac.id>
2024-06-11 03:12:02 -0400

    add control flow

---
+++ control_flow.js
@@ -0,0 +1,43 @@
+let userRole = "admin";
+let isLoggedIn = true;
+let userType = "subscriber";
+
+let accessLevel;
+let userMessage;
+let userCategory;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if ( userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+if (isLoggedIn){
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to acces the system";
+}
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("Acces Level:", accessLevel);
+console.log("User Message:", userMessage);
+console.log("User Category:", userCategory);
